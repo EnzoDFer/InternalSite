91541096d8f2530d93db4d769c8eb80be85b0134
enzodfer <enzodfer@outlook.com>
2022-11-07 13:46:23 -0500

    Create custom divider

--- src/Components/Section.tsx
+++ src/Components/Section.tsx
@@ -1,13 +1,10 @@
 import { ReactNode } from "react";
 import styles from "./Section.module.css";
-import { Divider } from "@chakra-ui/react";
 
 export const Section = ({children}:{children:ReactNode}) => {
   return (
-    <section
-      className={styles.sectionWrapper}
-    >
-      <Divider/>
+    <section className={styles.sectionWrapper} >
+      <hr className={styles.divider} />
       {children}
     </section>
   );
